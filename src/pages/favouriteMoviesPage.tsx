3d9883e0db92994e87a1aacd3fc15144acc6d70c
Ludmila20108895 <156093798+Ludmila20108895@users.noreply.github.com>
2025-08-08 14:23:12 +0100

    Refactor to allow for new filtering hook, and add the hook.

--- src/pages/favouriteMoviesPage.tsx
+++ src/pages/favouriteMoviesPage.tsx
@@ -1,19 +1,59 @@
-import React from "react"; // Import React for functional component
+import React from "react";
 import PageTemplate from "../components/templateMovieListPage";
+import useFiltering from "../hooks/useFiltering";
+import MovieFilterUI, {
+  titleFilter,
+  genreFilter,
+} from "../components/movieFilterUI";
+
+const titleFiltering = {
+  name: "title",
+  value: "",
+  condition: titleFilter,
+};
+const genreFiltering = {
+  name: "genre",
+  value: "0",
+  condition: genreFilter,
+};
 
 const FavouriteMoviesPage: React.FC = () => {
+  const { filterValues, setFilterValues, filterFunction } = useFiltering([
+    titleFiltering,
+    genreFiltering,
+  ]);
+
+  const changeFilterValues = (type: string, value: string) => {
+    const changedFilter = { name: type, value: value };
+    const updatedFilterSet =
+      type === "title"
+        ? [changedFilter, filterValues[1]]
+        : [filterValues[0], changedFilter];
+    setFilterValues(updatedFilterSet);
+  };
+
+  const favouriteMovies = JSON.parse(
+    localStorage.getItem("favourites") || "[]"
+  );
+
+  const displayedMovies = filterFunction(favouriteMovies);
+
   const toDo = () => true;
-  // Get movies from local storage.
-  const movies = JSON.parse(localStorage.getItem("favourites") || "[]"); // Parse the favourites from local storage or use an empty array if none exist.
 
   return (
-    <PageTemplate
-      title="Favourite Movies"
-      movies={movies}
-      selectFavourite={toDo} // Function to handle selecting a favourite movie, currently a placeholder
-    />
+    <>
+      <PageTemplate
+        title="Favourite Movies"
+        movies={displayedMovies}
+        selectFavourite={toDo}
+      />
+      <MovieFilterUI
+        onFilterValuesChange={changeFilterValues}
+        titleFilter={filterValues[0].value}
+        genreFilter={filterValues[1].value}
+      />
+    </>
   );
 };
 
 export default FavouriteMoviesPage;
-// This page displays the user's favourite movies.
