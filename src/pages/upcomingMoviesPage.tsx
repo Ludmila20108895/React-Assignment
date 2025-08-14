be8c6f96f1c45ad1b5fb0d0995e3b684bfd63c9d
Ludmila20108895 <156093798+Ludmila20108895@users.noreply.github.com>
2025-08-14 01:40:05 +0100

    New button for Upcoming movies page.

--- src/pages/upcomingMoviesPage.tsx
+++ src/pages/upcomingMoviesPage.tsx
@@ -2,22 +2,34 @@ import React, { useState, useEffect } from "react";
 import PageTemplate from "../components/templateMovieListPage";
 import { BaseMovieProps } from "../types/interfaces";
 import { getUpcomingMovies } from "../api/tmdb-api";
-import AddToFavouritesIcon from "../components/cardIcons/addToFavourites";
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+import IconButton from "@mui/material/IconButton";
 
 const UpcomingMoviesPage: React.FC = () => {
   const [movies, setMovies] = useState<BaseMovieProps[]>([]);
+
+  // Function to add a movie to the "Must Watch" list
+  const AddMustWatchIcon = (movie: BaseMovieProps) => {
+    return (
+      <IconButton
+        aria-label="add to must watch"
+        onClick={() => console.log(`Add ${movie.title} to must watch list`)}
+      >
+        <PlaylistAddIcon color="primary" fontSize="large" />
+      </IconButton>
+    );
+  };
 
   useEffect(() => {
     getUpcomingMovies().then((data) => {
       setMovies(data.results);
     });
   }, []);
-
   return (
     <PageTemplate
       title="Upcoming Movies"
       movies={movies}
-      action={(movie: BaseMovieProps) => <AddToFavouritesIcon movie={movie} />}
+      action={AddMustWatchIcon}
     />
   );
 };
